d649a595a5f5f53936f63d80a028d55e6438a9c2
Euan Smith <euan1971@yahoo.co.uk>
2022-09-05 22:43:48 +0100

    added a pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  assetsInclude:['public/*.json','public/*.mp3']
+  plugins: [
+    vue(),
+    VitePWA({ registerType: 'autoUpdate' })
+  ],
+  // assetsInclude:['public/*.json','public/*.mp3']
 })
