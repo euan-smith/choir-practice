1b3113e2616dcc7564446fdf53e035e89bd868dc
Euan Smith <euan1971@yahoo.co.uk>
2022-09-06 21:09:58 +0100

    modified service worker caching options

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,38 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      workbox:{
+        runtimeCaching:[
+          {
+            handler:'StaleWhileRevalidate',
+            urlPattern:/^.*\.(mp3|json)$/,
+            options: {
+              cacheName: 'scores',
+              expiration: {
+                maxEntries: 20,
+                maxAgeSeconds: 180 * 24 * 60 * 60, // 180 days
+              },
+            },
+          },
+          {
+            handler:'CacheFirst',
+            urlPattern:/^.*\.js$/,
+            options: {
+              cacheName: 'scores',
+              expiration: {
+                maxEntries: 20,
+                maxAgeSeconds: 180 * 24 * 60 * 60, // 180 days
+              },
+            },
+          }
+        ]
+      }
+    })
   ],
   // assetsInclude:['public/*.json','public/*.mp3']
 })
