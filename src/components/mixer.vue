1cea079e7468ff14ec90e4bf6d66419c816a2d00
Euan Smith <euan1971@yahoo.co.uk>
2022-09-19 20:09:49 +0100

    added a back button which goes back to the last set bar

--- src/components/mixer.vue
+++ src/components/mixer.vue
@@ -74,7 +74,6 @@ export default {
       playing:false,
       firstBeat:null,
       startBeat:null,
-      endBeat:null,
       beat:null,
       beats:[],
       mainVol:1,
@@ -362,6 +361,11 @@ export default {
         this.setVols();
       }
     },
+    backToStart(){
+      this.pause();
+      console.log(this.startBeat, this.firstBeat);
+      this.gotoBar(this.startBeat || this.firstBeat);
+    },
     resize(){
       for(let n=0; n<this.parts.length; n++) this.$refs.partvol[n].setTo(this.tracks[n].vol);
     },
@@ -398,9 +402,13 @@ export default {
       spec.push(1);
       // first find all bars that meet the bar spec
       this.seekBar(spec);
+      this.startBeat = this.beat;
     },
     seekBar(spec){
-      this.beat = this.findBeat(spec) || this.beat;
+      this.gotoBar(this.findBeat(spec) || this.beat);
+    },
+    gotoBar(beat){
+      this.beat = beat;
       this.currentTime = this.beat.time;
       this.$refs.time.value = this.currentTime / this.duration;
     },
@@ -549,7 +557,7 @@ export default {
         <path :fill="metronome?.on ? '#eee' : '#555'" d="m 463.84136,154.89339 c -6.42,-6.42 -16.83,-6.42 -23.251,0 -71.31197,70.35135 -136.61146,132.25426 -208.741,199.7 h -105.82 c 23.35495,-140.1063 67.13099,-217.59716 120.727,-318.357996 0.86,-0.803 2.209,-0.801 3.067,-10e-4 20.50653,37.383983 48.51152,88.812606 72.26194,147.190756 1.186,9.002 12.2214,17.4338 23.3242,11.71391 9.002,-1.186 11.1594,-12.2324 9.9724,-21.2344 -21.69905,-53.89113 -30.43965,-85.078342 -83.11454,-161.702266 -13.446,-12.55299965 -34.508,-12.55699965 -47.954,10e-4 C 126.80877,149.30021 96.099465,324.74626 77.091365,474.25139 c -2.829,21.473 13.907,40.535 35.543995,40.535 h 271.311 c 21.661,0 38.373,-19.087 35.544,-40.535 -8.26237,-52.34207 -14.88466,-100.7074 -24.7871,-157.02622 -6.40949,-11.78839 -8.3911,-14.9907 -17.4031,-13.8037 -9.002,1.186 -13.59751,8.0528 -12.41051,17.0548 l 5.66371,34.11712 h -83.159 c 64.35441,-63.86663 129.29308,-130.29894 176.448,-176.449 6.42,-6.42 6.42,-16.83 -10e-4,-23.251 z m -88.956,232.582 12.004,91.074 c 0.112,0.846 -0.148,1.701 -0.708,2.341 -0.566,0.645 -1.38,1.014 -2.235,1.014 h -271.311 c -0.855,0 -1.668,-0.369 -2.231,-1.011 -0.564,-0.643 -0.824,-1.499 -0.712,-2.347 l 12.003,-91.072 h 253.19 z" />
       </svg>
       <svg class=reset @click=reset fill="#eee" viewBox="0 0 48 48"><path d="M26 18.5v-3h6.5V6h3v9.5H42v3ZM32.5 42V21.5h3V42Zm-20 0v-9H6v-3h16v3h-6.5v9Zm0-15V6h3v21Z"/></svg>
-      <svg class=back fill="#eee" viewBox="0 0 48 48"><path d="M11 36V12h3v24Zm26 0L19.7 24 37 12Z"/></svg>
+      <svg class=back @click=backToStart fill="#eee" viewBox="0 0 48 48"><path d="M11 36V12h3v24Zm26 0L19.7 24 37 12Z"/></svg>
       <label class=timeline>
         <input class=time ref=time type=range min=0 max=1 step=0.001 value=0 @input="setTime">
       </label>
